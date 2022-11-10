07813a21704309fd9f3f289fec5c40a0f53ebc6f
Christian Meier <christian@meier-lossburg.de>
2022-11-10 13:02:37 +0100

    deploy: fix

--- .devcontainer/build_armv7_prod.sh
+++ .devcontainer/build_armv7_prod.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
+# CAVE: execute in workdir 
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-
+ln -s build/prod_debian/build.json build.json
+ln -s build/prod_debian/config.json config.json
 
 docker run --rm --privileged \
     -v ~/.docker:/root/.docker \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
     -v $PWD/:/data tchr157/armv7-builder \
     --test \
-    --file build/prod_ubuntu/Dockerfile \
+    --file build/prod_debian/Dockerfile \
     --armv7 --target /data
+
+rm build.json config.json
  
---
+++ build/base_debian/rootfs/workspace/test_import.py
@@ -0,0 +1,26 @@
+import django
+import rest_framework
+import MySQLdb
+import pygments
+import zeroconf
+import asyncws
+import sqlalchemy
+import aiohttp
+import gunicorn
+import dash
+import ruamel.yaml
+import plotly
+import dash_daq
+import dash_html_components
+import dash_core_components
+import dash_bootstrap_components
+import dash_table
+import dask
+import dpd_components
+import django_plotly_dash
+import pandas
+import cryptography
+# Sci-kit learn
+import numpy
+import scipy
+import sklearn
